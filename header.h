82f7f27fe632873449e84a7d511db52296d4852e
HernanMontenegro <hernanmontenegroholberton@gmail.com>
2021-03-20 15:26:33 -0300

    instance function for not overweight main

--- header.h
+++ header.h
@@ -2,6 +2,15 @@
 #define HEADER_H
 
 #include <SDL2/SDL.h>
+#include <stdio.h>
 
+/* Struct for window instance  */
+typedef struct SDL_Instance
+{
+	SDL_Window *window;
+	SDL_Renderer *renderer;
+} SDL_Inst;
+
+int init_instance(SDL_Inst *);
 
 #endif /* HEADER_H  */
--- main.c
+++ main.c
@@ -2,33 +2,9 @@
 
 int main(void)
 {
-	SDL_Window *window;
-	SDL_Renderer *renderer;
+	SDL_Inst instance;
 
-	/* Initialize SDL  */
-	if (SDL_Init(SDL_INIT_VIDEO) != 0)
-	{
-		fprintf(stderr, "Unable to initialize SDL:  %s\n", SDL_GetError());
+	if (init_instance(&instance) != 0)
 		return (1);
-	}
-	/* Create a new Window Instance  */
-	window = SDL_CreateWindow("SDL2 ME GUSTA EL PITO", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1260, 720, 0);
-	if (!window)
-	{
-		fprintf(stderr, "SDL_CreateWindow Error:  %s\n", SDL_GetError());
-		SDL_Quit();
-		return (1);
-	}
-
-	/* Create a new Renderer instance linked to the Window  */
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (!renderer)
-	{
-		SDL_DestroyWindow(window);
-		fprintf(stderr, "SDL_CreateRenderer Error:  %s\n", SDL_GetError());
-		SDL_Quit();
-		return (1);
-	}
-
 	return (0);
 }
